41ada690a3047017893d2780bfd77ab28d43cd93
MiKi-06 <mehdikazemidev@gmail.com>
2026-06-07 23:21:54 +0330

    Added new columns to database
    Added 'source' column to database, and two functions to calculate ad's date (Still prototype)

--- car-advertisement-collector.py
+++ car-advertisement-collector.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from openpyxl import Workbook
 import sqlite3
 import re
+from jdatetime import datetime, timedelta
 import time
 
 def scroll_to_bottom(driver, pause_time=2, max_attempts = 5):
@@ -32,12 +33,38 @@ def get_digit(string_data):
   except ValueError:
     return 0
 
+def fa_to_en(text):
+    return text.translate(str.maketrans(
+        "۰۱۲۳۴۵۶۷۸۹",
+        "0123456789"
+    ))
 
+def get_date(date):
+  date = fa_to_en(date.strip())
+  now = datetime.now()
+  try:
+    date = datetime.strptime(date, "%Y/%m/%d")
+    return date.strftime("%Y-%m-%d %H:%M:%S")
+  except ValueError:
+    if "لحظاتی پیش" in date:
+      return now.strftime("%Y-%m-%d %H:%M:%S")
+    elif "ساعت" in date:
+      offset = int(re.sub(r"[^\d]", "", date))
+      delta = timedelta(hours=offset)
+      return (now - delta).strftime("%Y-%m-%d %H:%M:%S")
+    elif "دیروز" in date:
+      offset = 1
+      return (now - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+    elif "روز پیش" in date:
+      offset = int(re.sub(r"[^\d]", "", date))
+      return (now - timedelta(days=offset)).strftime("%Y-%m-%d %H:%M:%S")
+    return now.strftime("%Y-%m-%d %H:%M:%S")
+    
 wb = Workbook()
 URL = "https://bama.ir/car/all/fars-shiraz?installment=0&price=300000000,1500000000&body=passenger_car"
 title_selector = "div.inline-flex.mb-1 span.text-neutral-10"
 price_selector = 'p.flex.items-center.justify-end.gap-1'
-#condition_selector = '//*[@id="__nuxt"]/main/div[1]/div/section/section/div/article[1]/a/div/div/div[1]/div[1]/div[1]/div[3]/span/span[3]'
+date_selector = "//span[contains(@class, 'inline-block') and contains(@class, 'text-right') and contains(@class, 'w-full') and contains(@class, 'text-xs')]"
 try:
   driver = webdriver.Edge()
   driver.get(URL)
@@ -57,13 +84,14 @@ try:
     # Connecting to database
     with sqlite3.connect(r"output\\bama_scraping.db") as conn:
       cursor = conn.cursor()
-      # DATA TYPES NEED TO BE CHANGED IN FUTURE
       cursor.execute("""CREATE TABLE IF NOT EXISTS ads (
                         id INTEGER PRIMARY KEY AUTOINCREMENT,
                         title TEXT NOT NULL,
                         milage INTEGER,
                         price INTEGER,
-                        link TEXT UNIQUE);""")
+                        link TEXT UNIQUE,
+                        date TEXT,
+                        source TEXT);""")
 
       # Finds and inserts ads information into xlsx and database
       for i, article in enumerate(articles):
@@ -73,12 +101,12 @@ try:
           link = article.find_element(By.TAG_NAME, "a").get_attribute("href")
           price = article.find_element(By.CSS_SELECTOR, price_selector).text
           milage = article.find_element(By.CSS_SELECTOR, "span[dir='ltr']").text
-          
+          date = article.find_element(By.XPATH, date_selector).text
           price = get_digit(price)
           milage = get_digit(milage)
-
-          cursor.execute("""INSERT OR IGNORE INTO ads(title, milage, price, link)
-                            VALUES(?, ?, ?, ?);""", (title, milage, price, link))
+          date = get_date(date)
+          cursor.execute("""INSERT OR IGNORE INTO ads(title, milage, price, link, date, source)
+                            VALUES(?, ?, ?, ?, ?, ?);""", (title, milage, price, link, date, "bama"))
         except Exception as e:
           print(f"Error in article {i}: {e}")
           continue
@@ -93,7 +121,7 @@ try:
   
 
   #
-  time.sleep(10)
+  time.sleep(5)
 finally:
   wb.save(r"output\\ads.xlsx")
   driver.quit()
