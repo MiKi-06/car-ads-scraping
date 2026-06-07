23fb8d5f83abc9fb6ac54fea709e3693a5015845
MiKi-06 <mehdikazemidev@gmail.com>
2026-06-07 19:06:13 +0330

    SQLite3 data saving
    It will now save the data in a database using 'SQLite3' module

--- car-advertisement-collector.py
+++ car-advertisement-collector.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from openpyxl import Workbook
+import sqlite3
+import shutil
 import time
 
 def scroll_to_bottom(driver, pause_time=2, max_attempts = 5):
@@ -33,27 +35,48 @@ try:
   driver.get(URL)
   time.sleep(2)
   scroll_to_bottom(driver= driver)
-
+  # Gets ads containers
   articles = driver.find_elements(By.TAG_NAME, "article")
+  # Sets up the xlsx sheet
   sheet = wb.active
   sheet.title = 'CAR ads Data'
-
   sheet["A1"] = "Title"
   sheet["B1"] = "Condition"
   sheet["C1"] = "Price"
   sheet["D1"] = "Link"
-  for i, article in enumerate(articles):
-    title = article.find_element(By.CSS_SELECTOR, title_selector).text
-    link = article.find_element(By.TAG_NAME, "a").get_attribute("href")
-    price = article.find_element(By.CSS_SELECTOR, price_selector).text
-    condition = article.find_element(By.CSS_SELECTOR, "span[dir='ltr']").text
 
-    sheet[f"A{i+2}"] = title
-    sheet[f"B{i+2}"] = condition
-    sheet[f"C{i+2}"] = price
-    sheet[f"D{i+2}"] = link
+  # Finds and inserts ads information into xlsx and database
+  try:
+    with sqlite3.connect(r"output\\bama_scraping.db") as conn:
+      for i, article in enumerate(articles):
+        cursor = conn.cursor()
+        # DATA TYPES NEED TO BE CHANGED IN FUTURE
+        cursor.execute("""CREATE TABLE IF NOT EXISTS ads (
+                       id INTEGER PRIMARY KEY AUTOINCREMENT,
+                       title TEXT NOT NULL,
+                       milage TEXT,
+                       price TEXT,
+                       link TEXT UNIQUE);""")
+        conn.commit()
+        # Finds the elements
+        title = article.find_element(By.CSS_SELECTOR, title_selector).text
+        link = article.find_element(By.TAG_NAME, "a").get_attribute("href")
+        price = article.find_element(By.CSS_SELECTOR, price_selector).text
+        condition = article.find_element(By.CSS_SELECTOR, "span[dir='ltr']").text
+        cursor.execute("""INSERT INTO ads(title, milage, price, link)
+                          VALUES(?, ?, ?, ?);""", (title, condition, price, link))
+        sheet[f"A{i+2}"] = title
+        sheet[f"B{i+2}"] = condition
+        sheet[f"C{i+2}"] = price
+        sheet[f"D{i+2}"] = link
+  except sqlite3.OperationalError as e:
+    print(e)
+    pass
+  
 
+  #
   time.sleep(10)
 finally:
+  conn.close()
   wb.save(r"output\\ads.xlsx")
   driver.quit()
