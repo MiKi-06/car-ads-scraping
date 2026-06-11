fa054f1634e93fc5e71a3b0bb476c89e7d491bc7
MiKi-06 <mehdikazemidev@gmail.com>
2026-06-11 19:36:00 +0330

    Added get_good_deals() in analysis.py
    It receive ads as a required argument, also median, car model as optional arguments; Iterates through ads and appends ads  with prices less than (median*0.9) to good_deals list.

--- scraper/analysis.py
+++ scraper/analysis.py
@@ -20,10 +20,18 @@ def analyze_market(model="رنو، تندر 90"):
       "lowest": lowest,
       "average": average
     })
+    find_good_deals(ads, med_price, model)
   except Exception as e:
     print(e)
   finally:
     db.close()
 
-def find_good_deals(model="رنو، تندر 90"):
-  pass
+def find_good_deals(ads, med_price = None, model="رنو، تندر 90"):
+  good_deals = []
+  if not med_price:
+    prices = [row[3] for row in ads]
+    med_price = median(prices)
+  for ad in ads:
+    if ad[3] < (med_price * 0.9):
+      good_deals.append(ad)
+  print(good_deals)
